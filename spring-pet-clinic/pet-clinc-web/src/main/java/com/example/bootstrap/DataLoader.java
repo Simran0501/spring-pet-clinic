83a996e5af0accf9c40304451215f9c75350ed82
Simran0501 <simrangarg0501@gmail.com>
2023-02-24 23:06:03 +0530

    added PetTypes in bootstrap

--- spring-pet-clinic/pet-clinc-web/src/main/java/com/example/bootstrap/DataLoader.java
+++ spring-pet-clinic/pet-clinc-web/src/main/java/com/example/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.example.bootstrap;
 
 import com.example.model.Owner;
+import com.example.model.PetType;
 import com.example.model.Vet;
 import com.example.services.OwnerService;
+import com.example.services.PetTypeService;
 import com.example.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstname("Simran");
         owner1.setLastname("Garg");
