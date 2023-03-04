fa73112ee47792535fb83c5fc9f4ec5a203a5c42
Simran0501 <simrangarg0501@gmail.com>
2023-03-04 14:57:53 +0530

    implement owners to JPA entities

--- spring-pet-clinic/pet-clinc-data/src/main/java/com/example/model/Person.java
+++ spring-pet-clinic/pet-clinc-data/src/main/java/com/example/model/Person.java
@@ -1,7 +1,15 @@
 package com.example.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+
+    @Column(name = "first_name")
     private String firstname;
+
+    @Column(name = "last_name")
     private String lastname;
 
     public String getFirstname() {
